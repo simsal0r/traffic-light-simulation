4d1bad6676411c77f4a88411e7eaf3849c4faa39
Simeon <35310222+simsal0r@users.noreply.github.com>
2020-09-06 12:53:35 +0200

    Add request green button

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import "./styles.css";
 
 const circles = document.querySelectorAll(".circle");
+var lightInterval;
 
 //red/yellow, red, yellow, green
-var holdTimes = [1000, 3000, 1000, 3000];
+var holdTimes = [1000, 7000, 1000, 3000];
+var requestTime = 1000;
 
 //Start with green phase
 let activeLight = 2;
 var phase = 3;
 var remainingTime = holdTimes[phase];
-
-var requestTime = 0;
-
-var lightInterval;
 
 function runTrafficLight() {
   lightInterval = setInterval(() => {
@@ -29,13 +27,38 @@ function checkPhaseChange() {
     } else {
       phase--;
     }
-
+    console.log(phase);
     remainingTime = holdTimes[phase];
   } else {
     remainingTime = remainingTime - 100;
   }
   document.getElementById("counter").innerHTML = remainingTime;
 }
+
+function changeLight() {
+  clearLights();
+  //Special case red/yellow
+  if (phase === 1) {
+    const yellowLight = circles[1];
+    yellowLight.classList.add(yellowLight.getAttribute("color"));
+  } else {
+    if (activeLight <= 0) {
+      activeLight = 2;
+    } else {
+      activeLight--;
+    }
+  }
+
+  const currentLight = circles[activeLight];
+  currentLight.classList.add(currentLight.getAttribute("color"));
+}
+
+function clearLights() {
+  circles[activeLight].className = "circle";
+  circles[1].className = "circle";
+}
+
+//Buttons
 
 function startTrafficLight() {
   runTrafficLight();
@@ -59,29 +82,10 @@ function stopTrafficLight() {
   currentLight.classList.add(currentLight.getAttribute("color"));
 }
 
-function changeLight() {
-  clearLights();
-  //Special case red/yellow
+function substractRequestTime() {
   if (phase === 1) {
-    const yellowLight = circles[1];
-    yellowLight.classList.add(yellowLight.getAttribute("color"));
-  } else {
-    if (activeLight <= 0) {
-      activeLight = 2;
-    } else {
-      activeLight--;
-    }
+    remainingTime = remainingTime - requestTime;
   }
-
-  //console.log(activeLight);
-
-  const currentLight = circles[activeLight];
-  currentLight.classList.add(currentLight.getAttribute("color"));
-}
-
-function clearLights() {
-  circles[activeLight].className = "circle";
-  circles[1].className = "circle";
 }
 
 document
@@ -93,3 +97,9 @@ document
 document
   .getElementById("button-stop")
   .addEventListener("click", stopTrafficLight);
+document
+  .getElementById("button-request")
+  .addEventListener("click", substractRequestTime);
+
+document.getElementById("button-request").innerHTML +=
+  " (" + requestTime + " ms)";
